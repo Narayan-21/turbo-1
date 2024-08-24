be11524f5640bcbff933bf0855f9b9abab8da1b2
Narayan-21 <naaidjan.19@gmail.com>
2024-08-25 02:54:30 +0530

    nodejs app integration

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import Admin from "@repo/ui/admin";
+import { Inputbox } from "@repo/ui/inputbox";
+
+export default function() {
+    return <div>
+        Hi From admin page <br />
+        <Admin />
+        <Inputbox/>
+    </div>
+}
---
+++ packages/ui/src/admin.tsx
@@ -0,0 +1,5 @@
+export default function(){
+    return <div>
+        Hi this is Admin Page!!
+    </div>
+}
